2c335ffbd26a044e10ea2fe2c633e76b8a6c6212
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2024-04-12 20:06:58 -0300

    Add

--- downloadDataSUS.py
+++ downloadDataSUS.py
@@ -8,17 +8,20 @@ from pysus.online_data.SIH import download
 
 #DEFININDO VARIÁVEIS
 vars = ["MORTE", "CID_MORTE", "DIAG_PRINC"]
-ufs = ["sp"]
+ufs = ["df"]
 anos = [2021] 
-meses = [ 6, 7, 8, 9, 10, 11, 12] 
+meses = [ 11, 12] 
 cids = ["J13", "J14"]
+grupos = ['RD', 'RJ', 'ER', 'SP', 'CH', 'CM',]
+
 
 #IMPORTANDO DADOS
 for mes in meses: 
     for uf in ufs:
         for ano in anos:   
-            df = download(uf, ano, mes)
+            d = download(uf, ano, mes, grupos)
+            print(d)
+            df = pd.DataFrame(data=d)
             df = df.filter(vars)
-            df = df[df['DIAG_PRINC'].isin(cids)]
-            df.to_csv("sih_pneumonia_21.csv", mode='a', index=False, header=False)
-            print(f"O arquivo do mês {mes} de {ano} do estado {uf.upper()} foi filtrado")
+            df.to_csv("sih_pneumonia_20_21.csv", mode='a', index=False, header=False)
+            ##print(f"O arquivo do mês {mes} de {ano} do estado {uf.upper()} foi filtrado")
