029a992859f359a92cbf96db9a68954b50507c8c
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2024-04-12 20:57:30 -0300

    Corrected Download code

--- downloadDataSUS.py
+++ downloadDataSUS.py
@@ -19,9 +19,11 @@ grupos = ['RD', 'RJ', 'ER', 'SP', 'CH', 'CM',]
 for mes in meses: 
     for uf in ufs:
         for ano in anos:   
-            d = download(uf, ano, mes, grupos)
-            print(d)
-            df = pd.DataFrame(data=d)
+            d = download(uf, ano, mes, grupos,
+                         data_dir="/Users/febagnatori/Documents/GitHub/ScientificResearchAtibiotics/Dados")
+            frames = [d[i].to_dataframe() for i in range(0, len(d))]
+            df = pd.concat(frames)
             df = df.filter(vars)
+            print(df)
             df.to_csv("sih_pneumonia_20_21.csv", mode='a', index=False, header=False)
-            ##print(f"O arquivo do mês {mes} de {ano} do estado {uf.upper()} foi filtrado")
+            print(f"O arquivo do mês {mes} de {ano} do estado {uf.upper()} foi filtrado")
