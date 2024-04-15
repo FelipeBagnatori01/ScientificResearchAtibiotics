7dd1bb508eaf41d383444edc5ca47e04bf56ad02
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2024-04-15 19:49:53 -0300

    Update downloadDataSUS.py

--- downloadDataSUS.py
+++ downloadDataSUS.py
@@ -7,4 +7,5 @@ sih = SIH().load()
 files = sih.get_files("RD", uf="SP", year=2000, month=[1,2,3])
 sih.download(files, local_dir="/Users/febagnatori/Documents/GitHub/ScientificResearchAtibiotics/Dados")
 parquet = sih.download(files)[0]
-print(parquet.to_dataframe())
+df = parquet.to_dataframe()
+print(df)
