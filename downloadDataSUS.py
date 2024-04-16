9ce8b683b1b3dc093040bc3892ef48634da9a839
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2024-04-16 18:01:48 -0300

    Update downloadDataSUS.py

--- downloadDataSUS.py
+++ downloadDataSUS.py
@@ -2,8 +2,9 @@
 import pandas as pd   
 
 from pysus.ftp.databases.sih import SIH
-sih = SIH().load()
 
+
+sih = SIH().load()
 files = sih.get_files("RD", uf="SP", year=2020, month=[1,2,3])
 sih.download(files, local_dir="/Users/febagnatori/Documents/GitHub/ScientificResearchAtibiotics/Dados")
 parquet = sih.download(files)[0]
