a3cdb3d5dc56dd653d258c294f188d7f9d5cb5ee
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2024-04-16 19:11:07 -0300

    Delete Idade.py

--- Idade.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-lista = []
-
-for i in range(0, 10):
-    aux = []
-    for j in range(0, 10):
-        aux.append(str(i) + " " + str(j))
-    lista.append(aux)
-print(np.array(lista))
