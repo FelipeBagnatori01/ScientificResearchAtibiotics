dee5eed7a0f9c361cd9eebd274e98d5b37057242
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2024-05-09 17:34:59 -0300

    Training the model and improving download Function

--- downloadDataSUS.py
+++ downloadDataSUS.py
@@ -5,6 +5,7 @@ from pysus.ftp.databases.sih import SIH
 def downloadData(uf, years, months, local_dir, group="RD"):
     sih = SIH().load()
     df = pd.DataFrame(data={})
+    
     for year in years:
         files = sih.get_files(group, uf=uf, year=year, month=months)
         parquet = sih.download(files, local_dir=local_dir)
@@ -13,6 +14,7 @@ def downloadData(uf, years, months, local_dir, group="RD"):
             dfAux = parquet[i].to_dataframe()
             df0 = pd.concat([df0, dfAux], ignore_index=True)
         df = pd.concat([df, df0], ignore_index=True)
+    
     return df
 
 df = downloadData("SP", [2020, 2021], [1, 2, 3], "/Users/febagnatori/Documents/GitHub/ScientificResearchAtibiotics/Dados")
